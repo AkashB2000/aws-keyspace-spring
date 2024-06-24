3df0ac40072dc622c9b606d9f0741eb22f2e5a50
AkashB2000 <bhargavakash23@gmail.com>
2024-06-24 18:02:01 +0530

    added queries

--- keyspace/src/main/java/com/keyspace/connect/keyspace/config/CassandraConfig.java
+++ keyspace/src/main/java/com/keyspace/connect/keyspace/config/CassandraConfig.java
@@ -3,6 +3,9 @@ package com.keyspace.connect.keyspace.config;
 import com.datastax.dse.driver.api.core.auth.ProgrammaticDseGssApiAuthProvider;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.aws.mcs.auth.SigV4AuthProvider;
@@ -13,6 +16,7 @@ import java.net.InetSocketAddress;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 @Configuration
 public class CassandraConfig {
@@ -20,22 +24,28 @@ public class CassandraConfig {
     public CqlSession session() throws NoSuchAlgorithmException {
         //File driverConfig = new File(System.getProperty("user.dir") + "/application.conf");
 
-        SigV4AuthProvider provider = new SigV4AuthProvider("ap-south-1");
+        SigV4AuthProvider provider = new SigV4AuthProvider("us-east-1");
         DriverConfigLoader loader = DriverConfigLoader.fromClasspath("application.conf");
-        return CqlSession.builder()
+        CqlSession session= CqlSession.builder()
                 .withConfigLoader(loader)
                 .withKeyspace("perinataldatastore") // Replace with your keyspace name
                 .build();
 
-//        List<InetSocketAddress> contactPoints = Collections.singletonList(
-//                        InetSocketAddress.createUnresolved("cassandra.us-east-2.amazonaws.com", 9142));
-//
-//        try (CqlSession session = CqlSession.builder()
-//                .addContactPoints(contactPoints)
-//                .withSslContext(SSLContext.getDefault())
-//                .withLocalDatacenter("us-east-2")
-//                .withAuthProvider(provider)
-//                .build()) {
-//            // App code here...
+        String query = "SELECT * FROM perinataldatastore.user LIMIT 10"; // Replace with your table name and query
+        ResultSet resultSet = session.execute(SimpleStatement.newInstance(query));
+
+        // Print the results
+        for (Row row : resultSet) {
+            UUID id = row.getUuid("id");
+            String name = row.getString("name");
+            int age = row.getInt("age");
+            System.out.println("ID: " + id + ", Name: " + name + ", Age: " + age);
+
+        }
+
+        return session;
+
+
+
         }
     }
